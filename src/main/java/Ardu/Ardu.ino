2f93b37cdef5d1524e0a48c0b190932c4590053c
orsenigo_giacomo <giacomo.orsenigo@gmail.com>
2020-01-15 08:34:01 +0100

    Led Arduino

--- src/main/java/Ardu/Ardu.ino
+++ src/main/java/Ardu/Ardu.ino
@@ -5,6 +5,8 @@
 #define RESET_DIO 9
 #define delayRead 1000 // Time of delay 
 #define buzzerPin 4
+#define LED_VERDE 7
+#define LED_ROSSO 6
 
 /* Viene creata una istanza della RFID libreria */
 RFID RC522(SDA_DIO, RESET_DIO);
@@ -19,6 +21,9 @@ void setup()
   /* Viene inizilizzato RFID reader */
   RC522.init();
   Serial.println("Setup");
+  pinMode(LED_VERDE,OUTPUT);
+  pinMode(LED_ROSSO,OUTPUT);
+  digitalWrite(LED_ROSSO,HIGH);
 }
 
 void loop()
@@ -43,6 +48,12 @@ void loop()
     if (codiceLetto.length() == 10) {
       Serial.println(codiceLetto);
       tone(buzzerPin, 3000, 500);
+      
+      digitalWrite(LED_ROSSO,LOW);
+      digitalWrite(LED_VERDE,HIGH);
+      delay(delayRead);
+      digitalWrite(LED_ROSSO,HIGH);
+      digitalWrite(LED_VERDE,LOW);
     }else
       tone(buzzerPin, 5000, 500);
     
